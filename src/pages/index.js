bcd995c4c9623b8a7347fb2c438b421f85d402ff
Christiaan Quyn <christianq010@gmail.com>
2018-01-26 22:26:48 +0530

    Lesson 05 - Filters & Sorting blog posts With GraphQL

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
   <div>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
@@ -9,7 +9,41 @@ const IndexPage = () => (
     <Link to="/page-2/">Go to page 2</Link>
     <p>Here is page 3</p>
     <Link to="/page-3/">Go to page 3</Link>
+    <h1>Blog Index</h1>
+    <ul>
+      {data.allMarkdownRemark.edges.map(post => (
+        <li>
+          <Link 
+            key={post.node.id} 
+            to={post.node.frontmatter.path}
+            >
+              {post.node.frontmatter.title}
+          </Link>
+        </li>
+      ))}
+    </ul>
   </div>
 )
+
+export const pageQuery = graphql `
+  query IndexQuery {
+    allMarkdownRemark ( 
+      limit: 10
+      sort: { fields: [frontmatter___date], order: ASC }
+      filter: { frontmatter: { published: {eq: true} } }
+    )
+    {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            path
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
